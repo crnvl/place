9ed5759d8c48e8fa827949e24a20cc11cd8c0592
angelsflyinhell <azami@namespace.media>
2023-02-17 21:57:52 +0100

    Add server sync

--- www/src/main.rs
+++ www/src/main.rs
@@ -1,12 +1,13 @@
 use gloo::utils::document;
-use wasm_bindgen::JsCast;
-use web_sys::Request;
-use yew::{function_component, Html, use_effect, html};
+use serde::{Deserialize, Serialize};
+use wasm_bindgen::{JsCast, JsValue};
+use yew::{function_component, html, use_effect, Html};
 
 fn main() {
     yew::Renderer::<App>::new().render();
 }
 
+#[derive(Deserialize, Serialize)]
 struct Point {
     x: i32,
     y: i32,
@@ -15,21 +16,37 @@ struct Point {
 
 #[function_component]
 fn App() -> Html {
-    use_effect(move ||{
-        let canvas = document()
-            .get_element_by_id("canvas")
-            .unwrap()
-            .dyn_into::<web_sys::HtmlCanvasElement>()
-            .unwrap();
+    use_effect(move || {
+        let timers = gloo::timers::callback::Interval::new(5000, || {
+            wasm_bindgen_futures::spawn_local(async {
+                let canvas = document()
+                    .get_element_by_id("canvas")
+                    .unwrap()
+                    .dyn_into::<web_sys::HtmlCanvasElement>()
+                    .unwrap();
 
-        let context = canvas.get_context("2d").unwrap().unwrap();
-        let context_2d = context
-            .dyn_into::<web_sys::CanvasRenderingContext2d>()
-            .unwrap();
+                let context = canvas.get_context("2d").unwrap().unwrap();
+                let context_2d = context
+                    .dyn_into::<web_sys::CanvasRenderingContext2d>()
+                    .unwrap();
 
-        let timers = gloo::timers::callback::Interval::new(5000, move || {
-            wasm_bindgen_futures::spawn_local(async move {
                 let res = reqwest::get("http://localhost/grid").await.unwrap();
+                res.json::<Vec<Point>>()
+                    .await
+                    .unwrap()
+                    .iter()
+                    .for_each(|point| {
+                        context_2d.set_fill_style(&JsValue::from_str(&format!(
+                            "rgb({},{},{})",
+                            point.color, point.color, point.color
+                        )));
+                        context_2d.fill_rect(
+                            (point.x * 10) as f64,
+                            (point.y * 10) as f64,
+                            10.,
+                            10.,
+                        );
+                    });
             })
         });
         timers.forget();
