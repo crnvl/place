581793bb864896db8fc2b478d9ee63a81890fb49
angelsflyinhell <azami@namespace.media>
2023-02-17 19:49:01 +0100

    Create Yew project

--- www/src/main.rs
+++ www/src/main.rs
@@ -1,3 +1,14 @@
+use yew::{html, function_component, Html};
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
+}
+
+#[function_component]
+fn App() -> Html {
+    html! {
+        <div>
+            <h1>{"Hello World!"}</h1>
+        </div>
+    }
 }
